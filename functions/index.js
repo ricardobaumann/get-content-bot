a747ace32d6672f9539669493ba8187a284c0a6a
Ricardo Baumann <gemini.richard@gmail.com>
2017-10-11 13:57:45 +0200

    first version

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,12 @@ process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').ApiAiApp;
 const functions = require('firebase-functions');
 
-
 // a. the action name from the make_name API.AI intent
 const NAME_ACTION = 'get_content';
 // b. the parameters that are parsed from the make_name intent 
 const CONTENT = 'content';
+
+const contentMap = {"sports" : "sports are very nice and healthy", "politiks" : "politiks are boring. try sports"};
 
 exports.content_bot = functions.https.onRequest((request, response) => {
   const app = new App({request, response});
@@ -18,9 +19,10 @@ exports.content_bot = functions.https.onRequest((request, response) => {
 
 // c. The function that generates the silly name
   function get_content (app) {
-    let given_name = app.getArgument(CONTENT);
-    app.tell('Alright, your silly name is ' + given_name +
-      '! I hope you like it. See you next time.');
+    let content = app.getArgument(CONTENT);
+    
+    app.tell('Alright,' + contentMap[content]);
+    
   }
   // d. build an action map, which maps intent names to functions
   let actionMap = new Map();
